8c6c77a47efb1c145dda73db886ae707e45bc8c1
KGON124 <onozaki.keigo.lo@tut.jp>
2025-06-04 11:01:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 export default defineConfig({
-  base: "/KGON124.github.io/",   // 必ず先頭と末尾にスラッシュ
+  base: "/",   // 必ず先頭と末尾にスラッシュ
   build: {
     outDir: "dist"      // Vite デフォルト dist/
   }
